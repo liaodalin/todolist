3f7af3fc58e8f5162a18d3b417137b60b8fd7f14
Developer <dev@todolist.local>
2026-05-20 18:20:12 +0800

    fix: update build target to support modern JS features

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   },
   envPrefix: ["VITE_", "TAURI_"],
   build: {
-    target: ["es2021", "chrome100", "safari13"],
+    target: ["es2022", "chrome110", "safari16"],
     minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
     sourcemap: !!process.env.TAURI_DEBUG,
   },
